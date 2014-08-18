827f1ab191be9189dc02859c0ff8bf5b17e15db4
Kairo <kairogyn@gmail.com>
2014-08-18 18:04:41 +0000

    feature login bug fix

--- ntalk/app.js
+++ ntalk/app.js
@@ -1,6 +1,7 @@
 var express = require("express"), load = require("express-load"), app=express();
 
-app.set('view', __dirname + '/public');
+app.set('views', __dirname + '/views');
+
 app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/public'));
